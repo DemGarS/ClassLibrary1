ffd6f67ff590f368301bb60b638978311ccca4b7
315 <315@315-37>
2024-12-24 10:55:27 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+ 
+    
+
+        public class Person
+        {
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public List<Hobby> Hobbies { get; private set; } = new List<Hobby>();
+            public void AddHobby(Hobby hobby)
+            {
+                Hobbies.Add(hobby);
+            }
+            public void ShowAll()
+            {
+                foreach (var hobby in Hobbies.OrderBy(h => h.Name)) //garshin 34IS
+                {
+                    hobby.Show();
+                }
+            }
+        }
+    }
+    public class Hobby
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public void Show()
+        {
+            System.Console.WriteLine($"Название: {Name}, Описание: {Description}");
+        }
+    }
+
+
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                var person = new Person
+                {
+                    Name = "Иван Иванов",
+                    Address = "Москва"
+                };
+                person.AddHobby(new Hobby { Name = "Чтение", Description = "Люблю читать книги" });
+                person.AddHobby(new Hobby { Name = "Футбол", Description = "Играю в футбол по выходным" });
+                person.AddHobby(new Hobby { Name = "Кино", Description = "Смотрю фильмы разных жанров" });
+                person.ShowAll();
+            }
+            Console.Write("Гаршин 34ис");
+            Console.ReadKey();
+
+        }
+    }
+}
